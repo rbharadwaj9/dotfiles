fbbc3966efc0e7e09d830ae0905cd83b0c5c4dfc
Rajiv Bharadwaj <rajivbh@umich.edu>
2022-12-18 17:35:44 -0500

    Swapped goto keybinds within lsp

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -7,8 +7,8 @@
 local rval = {}
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '<leader>aj', vim.diagnostic.goto_prev)
-vim.keymap.set('n', '<leader>ak', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>ak', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>aj', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
