fa306895622478de313bbbe5eb5522c77196adf5
Rajiv Bharadwaj <rajivbh@umich.edu>
2022-12-18 14:32:03 -0500

    Added ftplugin for java, new colorscheme, lua LSP for nvim api

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -4,6 +4,7 @@
 --
 -- The user will still be required to install the appropriate Language Server 
 -- on their own. See https://langserver.org
+local rval = {}
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>aj', vim.diagnostic.goto_prev)
@@ -78,7 +79,7 @@ end
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'jdtls', 'pyright', 'tsserver', 'sumneko_lua', 'gopls' }
+local servers = { 'clangd', 'pyright', 'tsserver', 'sumneko_lua', 'gopls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
@@ -100,6 +101,38 @@ end
 
 -- Turn on lsp status information
 require('fidget').setup{}
+
+-- Example custom configuration for lua
+--
+-- Make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+require('lspconfig').sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = { enable = false },
+    },
+  },
+}
+
+rval.on_attach = on_attach
+
+return rval
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
