ae58adde42a77314ed2fa52431826c78ac3bc102
Rajiv Bharadwaj <44103483+rbharadwaj9@users.noreply.github.com>
2025-05-03 17:56:45 +0200

    Updated nvim-tree config

--- .config/nvim/lua/plugins/filesystem.lua
+++ .config/nvim/lua/plugins/filesystem.lua
@@ -6,6 +6,9 @@ return {
             'nvim-tree/nvim-web-devicons',
         },
         opts = {
+            view = {
+                signcolumn = "auto",
+            },
             filters = {
                 dotfiles = false,
                 git_ignored = false,
@@ -13,13 +16,25 @@ return {
             },
             renderer = {
                 group_empty = true,
+                full_name = true,
             },
+            actions = {
+                open_file = {
+                    quit_on_open = true,
+                }
+            },
+            tab = {
+                sync = {
+                    open = true,
+                    close = true,
+                }
+            }
         },
         keys = {
             {'<leader>to', "<cmd>NvimTreeFocus<cr>", desc = "[T]ree [O]pen", nowait = true, silent = true},
             {'<leader>tt', "<cmd>NvimTreeToggle<cr>", desc = "[T]ree [T]oggle", nowait = true, silent = true},
             {'<leader>tc', "<cmd>NvimTreeClose<cr>", desc = "[T]ree [C]lose", nowait = true, silent = true},
             {'<leader>tf', "<cmd>NvimTreeFindFile<cr>", desc = "[T]ree [F]ind File", nowait = true, silent = true},
-        }
+        },
     },
 }
