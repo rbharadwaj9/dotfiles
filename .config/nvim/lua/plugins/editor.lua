333449ea668cbf73c9801d6972788fc0057e8857
Rajiv Bharadwaj <44103483+rbharadwaj9@users.noreply.github.com>
2025-05-20 00:17:21 +0200

    Experimenting with gitsigns author

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -58,7 +58,13 @@ return {
     {
         'lewis6991/gitsigns.nvim',
         event = "BufEnter",
-        config = true,
+        opts = {
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text_pos = 'right_align',
+                delay = 1000,
+            },
+        },
     },
     {
         'saghen/blink.cmp',
