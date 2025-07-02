b2358b44d8e98a04a4af65cc37b14eb42e54136f
Rajiv Bharadwaj <44103483+rbharadwaj9@users.noreply.github.com>
2025-07-02 13:44:13 +0200

    Fixed LSP loading bug

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,7 @@ return {
         ensure_installed = require("constants.lsp_servers").mason,
         automatic_enable = false -- Since we have a separate lsp loading script, prevents duplicate LSPs
       };
+      require("lsp")
     end,
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
@@ -16,10 +17,8 @@ return {
   },
   {
     'j-hui/fidget.nvim',
+    opts = {},
     event = "LspAttach",
-    config = function ()
-      require("lsp")
-    end
   },
 }
 
